33394442eb62f6990ddfb5250a1adc9ee56a3fc5
Geenons david <geenonsdavid@gmail.com>
2022-02-28 19:13:15 +0100

    Ajout fichier data.php

---
+++ data.php
@@ -0,0 +1,10 @@
+<?php $dataArticle = array($id => 0, $nom => "tarte", $price => 10, $number =>1,);
+?>
+
+$price = 10; /* prix d'un article */
+$number = 8; /* nombre d'article commander */
+$name = "Tarte"; /* nom de l'article */
+$montant= 3.85; /* montant de la commande */
+$total = 10; /* total de la commande */
+$devise = "€";
+$date = "22/02/2022";
