4acb7efa8fa3a67c76f5ef73a7a28ab5bac23911
Geenons david <geenonsdavid@gmail.com>
2022-02-28 19:12:33 +0100

    Ajout fichier configuration.php

---
+++ configuration.php
@@ -0,0 +1,11 @@
+<?php
+$version = 1;
+$price = 3.56; /* prix d'un article */
+$number = 8; /* nombre d'article commander */
+$name = "Tarte"; /* nom de l'article */
+$montant= $price * $number; /* montant de la commande */
+$total = $montant; /* total de la commande */
+$devise = "€";
+$date = "22/02/2022";
+$dateNow = "23/02/2022";
+?>
