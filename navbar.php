3f1dc94fae5c2fba416454e56e6880ee80f5e228
Geenons david <geenonsdavid@gmail.com>
2022-02-28 19:08:14 +0100

    Ajout fichier navbar.php

---
+++ navbar.php
@@ -0,0 +1,36 @@
+<nav class="row m-0">
+                <div class="col-4 ">
+                    <button class="btn btn-primary p-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-arrow-left-circle-fill" viewBox="0 0 16 16">
+                            <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0zm3.5 7.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z" />
+                        </svg>
+                    </button>
+
+                </div>
+                <div class="col-4 text-center">
+                    <div class="row">
+                        <h1>Achats</h1>
+                    </div>
+                    <div class="row">
+
+                        <h4><?php echo $dateNow ?></h4>
+                    </div>
+                </div>
+
+                <div class="col-4 text-end">
+                    <div>
+                        <div class="dropdown">
+                            <button class="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+                                    <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
+                                </svg>
+                            </button>
+                            <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                                <li><a class="dropdown-item" href="#">connexion</a></li>
+                                <li><a class="dropdown-item" href="#">A propos</a></li>
+                                <li><a class="dropdown-item" href="#">Version <?php echo $version ?></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </nav>
