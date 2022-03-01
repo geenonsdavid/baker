3aa912b332da0864db6f80fb1caff12bc7cf9b35
Geenons david <geenonsdavid@gmail.com>
2022-03-01 12:09:34 +0100

    Ajout lien CSS

--- index.php
+++ index.php
@@ -6,14 +6,15 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
-
+    <link rel="stylesheet" href="./style.css">
     <title>Baker</title>
 </head>
 <div class="container p-0">
 
-    <body class="bg-primary">
+    <body>
         <header>
             <?php include 'navbar.php'; ?>
         </header>
