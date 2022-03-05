8c1b220082e5842b32c78d3a2ef66cbb71cab860
Geenons david <geenonsdavid@gmail.com>
2022-03-05 19:24:08 +0100

    Mise en page

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php include 'configuration.php'?>
+<?php include 'configuration.php' ?>
 <!DOCTYPE html>
 <html lang="fr">
 
@@ -6,28 +6,27 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="./style.css">
     <title>Baker</title>
 </head>
-<div class="container p-0">
-<header>
-            <?php include 'navbar.php'; ?>
-</header>
-    <body>
-        <main>
-            <?php include 'total.php'; ?>
-            <?php include 'listArticle.php'; ?>
-            
-        </main>
-    </body>
-<footer>
-    <button type="button" class="floatingButton">
-                <img src="./icons/plus-circle.svg" height="50px" width="50px" alt="">
-    </button>
-</footer>    
-</div>
+
+
+<body>
+    <header>
+        <?php include 'navbar.php'; ?>
+        <?php include 'total.php'; ?>
+    </header>
+    <main>
+        <?php include 'listArticle.php'; ?>
+    </main>
+    <footer>
+        <?php include 'footer.php'; ?>
+    </footer>
+</body>
+
+
 
 </html>
