0ba2a20df9f1561d09c3a2e082ea4b238bc5abba
Geenons david <geenonsdavid@gmail.com>
2022-03-08 08:36:53 +0100

    Controle saisie formulaire

--- modification.php
+++ modification.php
@@ -27,10 +27,14 @@ include 'configuration.php' ?>
         <form action="validModification.php" method="POST" class="need-validation text-center">
         <label class="form-label d-block name" for="name"><?php echo "$_GET[name]"?></label>
         <input type="hidden" name="name" value="<?php echo "$_GET[name]"?>">
+
         <label class="form-label d-block" for="number">Quantité :</label>
-        <input type="text" name="number" value="<?php echo "$_GET[number]"?>">
+        <input class="" type="number" name="number" value="<?php echo "$_GET[number]"?>" min="0" max="50" required>
+        <div class="valid-feedback">
+            invalide!
+        </div>
         <label class="form-label d-block" for="price">Prix/article :</label>
-        <input type="text" name="price" value="<?php echo "$_GET[price]"?>">
+        <input class="" type="number" name="price" value="<?php echo "$_GET[price]"?>" required>
         <input type="submit" class="btn d-block mx-auto border-brown my-2 bg-brown" value="Validez">
     </form>
         <?php
