7fed66d2f7c0bf864d6573175addc72e28917aff
Geenons david <geenonsdavid@gmail.com>
2022-03-09 19:31:58 +0100

    Mise à jour des données

--- data.php
+++ data.php
@@ -1,10 +1,19 @@
-<?php $dataArticle = array($id => 0, $nom => "tarte", $price => 10, $number =>1,);
+<?php 
+$version = 1;
+$articles = [
+    ["id" => 0,"name" => "Tartelette", "price" => 2.6],
+    ["id" => 1,"name" => "Pain maya", "price" => 2.4],
+    ["id" => 2,"name" => "Pain aux céréales", "price" => 2.4],
+    ["id" => 3, "name" => "Pain aux raisins", "price" => 1.5],
+    ["id" => 4, "name" => "Pain aux fruits", "price" => 2.4]
+    ];
+
+$orders = [
+    ["name" => $articles[2]["name"], "price" => $articles[2]["price"], "number" => 1],
+    ["name" => $articles[3]["name"], "price" => $articles[3]["price"], "number" => 2],
+    ["name" => $articles[4]["name"], "price" => $articles[4]["price"], "number" => 1],
+    ["name" => $articles[0]["name"], "price" => $articles[0]["price"], "number" => 1],
+    ["name" => $articles[1]["name"], "price" => $articles[1]["price"], "number" => 1],
+];
 ?>
 
-$price = 10; /* prix d'un article */
-$number = 8; /* nombre d'article commander */
-$name = "Tarte"; /* nom de l'article */
-$montant= 3.85; /* montant de la commande */
-$total = 10; /* total de la commande */
-$devise = "€";
-$date = "22/02/2022";
