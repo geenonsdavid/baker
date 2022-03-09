d832405039c6d87fbdd0dc1de9facea3631efa70
Geenons david <geenonsdavid@gmail.com>
2022-03-09 20:05:06 +0100

    Ajout commentaire

--- index.php
+++ index.php
@@ -1,7 +1,10 @@
 <?php
-// parameter page
+//titre de la page
 $title = 'Achats';
 include_once 'configuration.php';
+// header
 include 'layout/head.php';
+//main
 include 'listArticle.php';
+//footer
 include_once './layout/footer.php'; ?>
